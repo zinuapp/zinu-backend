5523625cb860423a7b09227983a82feab9393c87
Douglas Eric Fonseca Rodrigues <dougefr@gmail.com>
2021-11-17 16:59:26 -0300

    implementando POC

--- src/main/java/com/github/zinuapp/backend/PassengerWebsocket.java
+++ src/main/java/com/github/zinuapp/backend/PassengerWebsocket.java
@@ -3,10 +3,14 @@ package com.github.zinuapp.backend;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import io.lettuce.core.GeoArgs;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulConnection;
 import io.lettuce.core.pubsub.StatefulRedisPubSubConnection;
 import io.lettuce.core.pubsub.api.reactive.ChannelMessage;
 import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.type.Argument;
 import io.micronaut.websocket.WebSocketSession;
+import io.micronaut.websocket.annotation.OnClose;
 import io.micronaut.websocket.annotation.OnMessage;
 import io.micronaut.websocket.annotation.OnOpen;
 import io.micronaut.websocket.annotation.ServerWebSocket;
@@ -16,7 +20,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 
-import java.util.stream.Collectors;
+import java.util.ArrayList;
 
 @ServerWebSocket("/ws/chat/{passengerId}")
 @Requires(env = "passenger")
@@ -25,7 +29,7 @@ public class PassengerWebsocket {
 	private static final Logger LOG = LoggerFactory.getLogger(PassengerWebsocket.class);
 
 	@Inject
-	StatefulRedisPubSubConnection<String, String> connection;
+	RedisClient redisClient;
 
 	@OnOpen
 	public Publisher<String> onOpen(String passengerId, WebSocketSession session) {
@@ -38,37 +42,61 @@ public class PassengerWebsocket {
 		throws JsonProcessingException {
 		LOG.info("Received from passenger [passengerId={}]: {}", passengerId, message);
 
-		if(message.startsWith("FIND_CARS")) {
-			var lat = message.split("\s")[1];
-			var longi = message.split("\s")[2];
+		if (message.startsWith("FIND_CARS")) {
+			var connection = redisClient.connect();
+			var lat = message.split("\\$")[1];
+			var longi = message.split("\\$")[2];
 
 			var commands = connection.sync();
 			var cars = commands.georadius("cars-geoloc", Double.parseDouble(longi),
 				Double.parseDouble(lat), 5, GeoArgs.Unit.km);
 
 			LOG.info("Cars found [passengerId={}]: {}", passengerId, cars.size());
+			connection.close();
+
 			return session.send(new JsonMapper().writeValueAsString(cars));
 		}
 
 		if (message.startsWith("GET_LOCATION")) {
-			var car = message.split("\s")[1];
+			var car = message.split("\\$")[1];
 
 			if (session.contains("GET_LOCATION_ALREADY_SUBSCRIBED_" + car)) {
 				return Flux.from(session.send("already subscribed to car " + car + " geolocation"))
 					.doOnNext(msg -> logMessageSentToPassenger(passengerId, msg));
 			}
 
+			var connection = redisClient.connectPubSub();
 			var reactive = connection.reactive();
 			reactive.subscribe(car + "-geoloc-topic")
 				.doOnSuccess(o -> session.put("GET_LOCATION_ALREADY_SUBSCRIBED_" + car, true))
+				.doFinally(o -> {
+					if(!session.contains("redis-connections")) {
+						session.put("redis-connections", new ArrayList<>());
+					}
+
+					session.get("redis-connections", Argument.listOf(StatefulRedisPubSubConnection.class))
+						.ifPresent(connections -> connections.add(connection));
+				})
 				.subscribe();
 
-			return reactive.observeChannels().map(ChannelMessage::getMessage)
+			return reactive.observeChannels()
+				.map(ChannelMessage::getMessage)
+				.map(coordinates -> car + "$" + coordinates)
 				.doOnNext(msg -> logMessageSentToPassenger(passengerId, msg))
 				.flatMap(session::send);
 		}
 
 		return Flux.from(session.send(message)).doOnNext(msg -> logMessageSentToPassenger(passengerId, msg));
+	}
+
+	@OnClose
+	public Publisher<String> onClose(String passengerId, WebSocketSession session) {
+		LOG.info("Passenger [passengerId={}] disconnected", passengerId);
+
+		session.get("redis-connections", Argument.listOf(StatefulRedisPubSubConnection.class))
+			.ifPresent(connections -> connections.forEach(StatefulConnection::close));
+
+		return session.send("bye");
 	}
 
 	private static void logMessageSentToPassenger(String passengerId, String message) {
